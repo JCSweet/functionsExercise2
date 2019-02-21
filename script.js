4c123349fb975249d3d2b30b85102eaea5dea4ee
JCSweet <sweetjor@gmail.com>
2019-02-21 13:33:21 -0500

    Second Commit: Part2 2-D

--- script.js
+++ script.js
@@ -2,36 +2,78 @@
 
 (function() {
 
-let charPosition = 0;
-let charDirection = 'east';
+let charXPosition = 0;
+let charYPosition = 0;
+let charDirection = 'north';
 
 function moveForward (forward) {
   if (charDirection === 'east') {
-    charPosition += forward;
-   } else {charPosition -= forward; 
+    charXPosition += forward;
+   } else if (charDirection === 'north') {
+     charYPosition += forward;
+   } else if (charDirection === 'west') {
+    charXPosition -= forward; 
+   } else if (charDirection === 'south') {
+    charYPosition -= forward; 
    }
 }
+
 function moveBackward (backward) {
+  
   if (charDirection === 'east') {
-    charPosition -= backward;
-} else {charPosition += backward;}
-}
-function turnAround () {
+    charXPosition -= backward;
+    } else if (charDirection === 'north') {
+      charYPosition -= backward;
+    } else if (charDirection === 'west') {
+    charXPosition += backward; 
+    } else if (charDirection === 'south') {
+    charYPosition += backward; 
+    }
+  }
+
+function turnRight () {
  if (charDirection === 'east') {
-    charDirection = 'west';
- } else {charDirection = 'east';
- }
+    charDirection = 'south';
+ } else if (charDirection === 'south') {
+   charDirection = 'west';
+ } else if (charDirection === 'west') {
+  charDirection = 'north';
+} else {charDirection = 'east'}
 }
+
+function turnLeft () {
+  if (charDirection === 'east') {
+     charDirection = 'north';
+  } else if (charDirection === 'north') {
+    charDirection = 'west';
+  } else if (charDirection === 'west') {
+   charDirection = 'south';
+ } else {charDirection = 'east'}
+ }
+
 function printLocation () {
-  console.log(charPosition);
+  if (charYPosition > 0 && charXPosition > 0) {
+  console.log(`${charYPosition} N, ${charXPosition} E`);
+  } else if(charYPosition < 0 && charXPosition > 0) {
+    console.log(`${charYPosition} N, ${charXPosition} E`);
+  } else if(charYPosition > 0 && charXPosition < 0) {
+    console.log (`${charYPosition} N, ${charXPosition} E`);
+  } else if(charYPosition < 0 && charXPosition < 0) {
+    console.log(`${charYPosition} N, ${charXPosition} E`);
+  } else {
+    console.log(`${charYPosition} N, ${charXPosition} E`);
+  }
 }
 
 moveForward(5);
-moveBackward(3);
+turnRight();
+moveForward(2);
 printLocation();
-turnAround();
-moveForward(10);
-moveBackward(5);
+turnLeft();
+turnLeft();
+moveForward(7);
+turnRight();
+moveBackward(3);
 printLocation();
 
 } )();
